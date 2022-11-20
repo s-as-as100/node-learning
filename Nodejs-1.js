42d8bdf40a87cfbce1a3e3d0e5cf7f2c5696a473
unknown <mohdarif013579@gmail.com>
2022-11-20 11:51:51 +0530

    basic node theory

---
+++ Nodejs-1.js
@@ -0,0 +1,37 @@
+//* what is node js ?
+
+// Node js is not a  langauge
+// This is server Environment
+// Node js can connect with database,
+// Code and syntax is very similar to javascript
+// But not exactly the same
+// node j free andd open source
+// Node js use chromoe's V8 engine to execute code
+
+
+//* Why do we need use node js ?
+
+// Node js mostly used for api
+// So we can connect the same database with web App, Mobile App
+// Node is easy to understand who know javascript
+// Node is super fast for api's
+// With Node and Javascript , you can become full stack developer
+
+
+//* How Node js use javascript ?
+
+// Inside chrome has V8 engine ( to execute code)
+// Node ke andar V8 engine use krliya 
+// v8 engine is written in C++ language 
+
+//* what Developer make with node js ?
+
+// Developers make api with node js
+// so we can connect server with client
+// Node can make api for web , android and ios etc
+// Make website also
+
+
+// Install and setup node js
+
+
