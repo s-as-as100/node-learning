e11c851a2b2f167153047615009849fc2d690922
unknown <mohdarif013579@gmail.com>
2022-11-21 15:32:23 +0530

    create mutiple file using cmg and show list

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
- const fs = require("fs");
- const input  = process.argv;
- if(input[2]=='add') {
-    fs.writeFileSync(input[3], input[4])
- }
- else if(input[2]=='remove') {
-    fs.unlinkSync(input[3])
- }
- else {
-    console.log("invalid input")
- }
+const fs = require("fs");
+
+const path = require("path");
+const dirPath = path.join(__dirname, "files");
+// create file
+
+// for ( i = 0; i <5; i++)
+//  {
+//     fs.writeFileSync(dirPath +" /hello" +i + ".txt","hello world")
+
+// }
+
+// read file
+
+fs.readdir(dirPath, (err, files) => {
+  console.warn(files);
+  files.forEach((item) => {
+    console.log(item);
+  });
+});
