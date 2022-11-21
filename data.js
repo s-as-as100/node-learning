e9ed00e5eb20c16ab9a9101675d46a2bf7397115
unknown <mohdarif013579@gmail.com>
2022-11-21 14:30:12 +0530

    make simple api

---
+++ data.js
@@ -0,0 +1,18 @@
+const data = [
+  {
+    name: "mohd",
+    email: "arif@gmail.com",
+    contact: 999890909,
+  },
+  {
+    name: "as",
+    email: "arif@gmail.com",
+    contact: 999890909,
+  },
+  {
+    name: "shoaib",
+    email: "arif@gmail.com",
+    contact: 999890909,
+  },
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,23 +1,10 @@
+// make simple api 
 const http = require('http');
-// http module handle server request and response.
-http.createServer((req,resp)=>{
-    // send data to client using resp
-    resp.write("hello world!ss");
+const data = require('./data')
+ http.createServer((req,resp) => {
+    // header
+    resp.writeHead(200, {'Content-Type': 'application/json'});
+    // body
+    resp.write(JSON.stringify(data))
     resp.end()
-}).listen(4500)
-// createServer take function as parameter
-
-function controller(req,resp) {
-    resp.write("<h1>Hello world!</h1>");
-    resp.end()
-}
-
-http.createServer(controller).listen(1000)
-
-
-// Node js is async 
-// 1. Run first Script
-// 2. Run second Script ( complex data )
-//-- it will not wait to finish 2nd script 
-// run third script 
-// continue 
+ }).listen(2000)
