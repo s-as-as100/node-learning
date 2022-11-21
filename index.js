4264f0e3513dd17d25ac8747f0970bdc2ae4df5f
unknown <mohdarif013579@gmail.com>
2022-11-21 15:14:57 +0530

    remove and add file using fs and command line

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// make simple api 
-const http = require('http');
-const data = require('./data')
- http.createServer((req,resp) => {
-    // header
-    resp.writeHead(200, {'Content-Type': 'application/json'});
-    // body
-    resp.write(JSON.stringify(data))
-    resp.end()
- }).listen(2000)
+ const fs = require("fs");
+ const input  = process.argv;
+ if(input[2]=='add') {
+    fs.writeFileSync(input[3], input[4])
+ }
+ else if(input[2]=='remove') {
+    fs.unlinkSync(input[3])
+ }
+ else {
+    console.log("invalid input")
+ }
