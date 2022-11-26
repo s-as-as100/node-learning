15a427006c2b6fe10c9922fb9cd21c35940915d2
unknown <mohdarif013579@gmail.com>
2022-11-26 18:37:09 +0530

    file system read , update

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const fs = require("fs");
 
 const path = require("path");
-const dirPath = path.join(__dirname, "files");
-// create file
+const dirPath =path.join(__dirname,'crud');
+const filePath=`${dirPath}/apple.txt`;
+ // create file
+//  fs.writeFileSync(filePath,'this is demo');
 
-// for ( i = 0; i <5; i++)
-//  {
-//     fs.writeFileSync(dirPath +" /hello" +i + ".txt","hello world")
+//Read file
+// fs.readFile(filePath,'utf-8',(err,item)=>{
+// console.log(item) 
+// })
+// Without Utf-8 parameter file read show like Buffer <Buffer 74 68 69 73 20 69 73 20 64 65 6d 6f>
 
-// }
+// Update file 
+// fs.appendFile(filePath,'and file ins apple.txt',(err) => {
+//     if(!err) {
+//         console.log('file is updated successfully')
+//     }
+// })
 
-// read file
+// rename
 
-fs.readdir(dirPath, (err, files) => {
-  console.warn(files);
-  files.forEach((item) => {
-    console.log(item);
-  });
-});
+// fs.rename(filePath,`${dirPath}/fruit.txt`,(err) => {
+//     if(!err) {
+//         console.log('file name is updated successfully')
+//     }
+// })
+
+// delete file
+
+fs.unlinkSync(`${dirPath}/fruit.txt`)
