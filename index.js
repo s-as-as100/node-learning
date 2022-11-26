3759c09ad3319ba4110a421595bef42776402513
unknown <mohdarif013579@gmail.com>
2022-11-26 19:35:11 +0530

    how node js works

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
- // Handle Asynchronous Data in Node js
-// let a=20;
-// let b=0;
-
-// let waitingData= new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(300)
-//      }, 2000);
-//  })
-
-//  waitingData.then((data)=>{
-//     b= data;
-//     console.log(a+b,"handle Asynchronous Data using Promise")
-//  })
-
-//  console.log(a+b)
-
+ // How Node js Works
+ // Call Stack      {
+ // Node Api           This is show Event loop
+ // Callback Queue    }
  
+ // call stack ka kam hai ye registeration karega ki konsa funtion
+ // Phle Call karega or konsa baad mein
+
+
+
+ // Call Stack 
+ // main () call if only single line of code run 
+ // main () out from call stack after call function is execute 
+ // If anyone funtion execution stop ,then it call out from call stack
+
+
+// Anything is outside the js is part of Node Api
+// like setTimeout(2sec) // part of c++
+// js:92% , C++ , c -> 8%
+
+
+
