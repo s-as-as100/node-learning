73304bfd5434f887d9392e3e15f39ad48f0230d4
unknown <mohdarif013579@gmail.com>
2022-11-28 17:10:07 +0530

    remove .html from client side for security

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 // What is Express js
 // Express js is framwork of node js ;
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+// path module is use for path for own project
 const app = express();
-const publicPath=path.join(__dirname, 'public')// to get [path] of public folder
+const publicPath = path.join(__dirname, "public"); // to get [path] of public folder
 
-// use is the function of express js 
-app.use(express.static(publicPath))
+// use is the function of express js
+// app.use(express.static(publicPath));
+
+app.get("/", (_, res) =>{
+    res.sendFile(`${publicPath}/index.html`);
+})
+app.get("/about", (_, res) =>{
+    res.sendFile(`${publicPath}/about.html`);
+})
+app.get("/home", (_, res) =>{
+    res.sendFile(`${publicPath}/home.html`);
+})
 
 // static load the content of static file
 
 // console.log(__dirname)// to get path of directory
-app.listen(3400)
-
-
-
-
-
-
-
-
-
+app.listen(3400);
