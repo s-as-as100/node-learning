728717f6f6942dd9d4633470eb808ac552febac7
unknown <mohdarif013579@gmail.com>
2022-11-28 17:04:09 +0530

    make html pages

--- index.js
+++ index.js
@@ -2,59 +2,17 @@
 // Express js is framwork of node js ;
 
 const express = require('express');
+const path = require('path');
 const app = express();
+const publicPath=path.join(__dirname, 'public')// to get [path] of public folder
 
-app.get('',(req,res)=>{
-    // req --> receive from client 
-    console.log('data receive from client', req.query);
-    // res -> send to client
-    res.send('Hello , this is home page')
-})
-app.get('/about',(req,res)=>{
-    // res.send('Hello , this is About page')
-    res.send(`<input type="text" name="" placeholder="hello"
-    <button type="submit>hero</button>
-    `)
+// use is the function of express js 
+app.use(express.static(publicPath))
 
-});
+// static load the content of static file
 
-app.get('/help',(req,res)=>{
-    res.send('Hello , this is Help page')
-});
-// restart server if add any code :
-app.listen(4000);
-
-// Render html tag;
-
-
-// Basic method of string;
-
-// 1.Length : Check the length of the string;
-const myString = ' I love you ';
-console.log(myString.length);
-
-//2 Trim : Remove whitespace at the beginning and at the end;
-console.log(myString.trim());
-
-// 3. Split : Convert a string to an array
-const array = myString.split('');
-console.log(array);
-
-// 4. Include: Return true if the string contains a particular 
-// string . In other case return false
-if(myString.includes('loves')) {
-    console.log('yes')
-}
-else {
-    console.log('no')
-}
-
-// 5. Char At : get the character at a specific position in a string :
-console.log(myString.charAt(5)) // --> v
-
-// Slice : Extract a part of a particular string :
-console.log(myString.slice(2,6))
-
+// console.log(__dirname)// to get path of directory
+app.listen(3400)
 
 
 
