b449fcd82012fb127517290860dad1a2f8a9cb37
unknown <mohdarif013579@gmail.com>
2022-11-28 18:25:11 +0530

    Template ejs

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require("path");
 // path module is use for path for own project
 const app = express();
 const publicPath = path.join(__dirname, "public"); // to get [path] of public folder
+app.set('view engine','ejs');
 
 // use is the function of express js
 // app.use(express.static(publicPath));
@@ -13,17 +14,21 @@ const publicPath = path.join(__dirname, "public"); // to get [path] of public fo
 app.get("/", (_, res) =>{
     res.sendFile(`${publicPath}/index.html`);
 })
-app.get("/about", (_, res) =>{
-    res.sendFile(`${publicPath}/about.html`);
-})
-app.get("/home", (_, res) =>{
-    res.sendFile(`${publicPath}/home.html`);
-})
-app.get("*", (_, res) =>{
-    res.sendFile(`${publicPath}/noPage.html`);
-})
+ 
+// What is template engine used for?
+// A template engine enables you to use static template files in your
+//  application. At runtime, the template engine replaces 
+// variables in a template file with actual values, and transforms the 
+// template into an HTML file sent to the client. This approach makes it 
+// easier to design an HTML page
 
-// static load the content of static file
+app.get("/profile", (_, res) =>{
+    let user = {
+        name:'Mohd arif',
+        profession:'Associate Software Development Engineer',
+        city:'New Delhi'
+    }
+    res.render('profile',{user})
+ })
 
-// console.log(__dirname)// to get path of directory
-app.listen(3400);
+ app.listen(3000)
