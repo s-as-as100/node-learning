e5f500d28d570a95fa0a5d5a4b9d69e90f6d5547
unknown <mohdarif013579@gmail.com>
2022-11-30 10:30:20 +0530

    Basic Middleware

--- index.js
+++ index.js
@@ -1,70 +1,33 @@
-// What is Express js
-// Express js is framwork of node js ;
-
-const express = require("express");
-const path = require("path");
-// path module is use for path for own project
+// Middleware
+// It is used for modify response
+// Easy to apply
+ const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "public"); // to get [path] of public folder
-app.set('view engine','ejs');
 
-// use is the function of express js
-// app.use(express.static(publicPath));
+app.get("/", (req, res) => {
+  res.send("Welcome to Home Page");
+});
 
-app.get("/", (_, res) =>{
-    res.sendFile(`${publicPath}/index.html`);
-})
- 
-// What is template engine used for?
-// A template engine enables you to use static template files in your
-//  application. At runtime, the template engine replaces 
-// variables in a template file with actual values, and transforms the 
-// template into an HTML file sent to the client. This approach makes it 
-// easier to design an HTML page
+const requestMidddleware = (req, res, next) => {
+  if (!req.query.age) {
+    res.send("Please enter age to access this page.");
+  } else if (req.query.age < 18) {
+    res.send("Please enter the valid age.");
+  } else {
+    next();
+  }
+};
+app.use(requestMidddleware);
 
-app.get("/profile", (_, res) =>{
-    let user = {
-        name:'Mohd arif', 
-        profession:'Associate Software Development Engineer',
-        city:'New Delhi',
-        skills:['html','Css', 'Javascript','React js', 'Antd','Firebase']
-    }
-    res.render('profile',{user})
- })
- app.get("/login", (_, res) =>{
-    let user = {
-        name:'Mohd arif', 
-        profession:'Associate Software Development Engineer',
-        city:'New Delhi',
-        skills:['html','Css', 'Javascript','React js', 'Antd','Firebase']
-    }
-    res.render('login',{user})
- })
- app.listen(3000);
+app.get("/users", (req, res) => {
+  res.send("Welcome to Users Page");
+});
 
+app.listen(3000);
 
-
-
-
-
-
-
-
-
-// hello 1
-// 110
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Types of Middleware 
+// 1. Application middleware
+// 2. Router Level middleware
+// 3. Error handling Middleware
+// 4. Build-in middleware
+// 5. Third party middleware
