77eb08e6d48ca3865927ab4b2e33f242c9537d4a
unknown <mohdarif013579@gmail.com>
2022-12-03 19:11:52 +0530

    add , update and delete operation with static data

---
+++ delete.js
@@ -0,0 +1,11 @@
+const dbConnect = require('./mongodb');
+
+const deleteData = async() =>{
+    const data = await dbConnect();
+    const result = await data.deleteMany({name:'note pro 5'})
+    console.log(result);
+    if(result.acknowledged) {
+        console.log('delete data successfully')
+    }
+ }
+deleteData()
---
+++ update.js
@@ -0,0 +1,16 @@
+const dbConnect = require("./mongodb");
+
+const updateData = async () => {
+  let data = await dbConnect();
+  // updateOne -> ony update single data if all same name:
+  // UpdateMany ->  update all
+  let result = await data.updateOne(
+    // first object use for condition {
+    { name: "note 5" },
+    {
+      $set: { name: "max pro 5T", price:1 },
+    }
+  );
+  console.log(result);
+};
+updateData();
