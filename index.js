998929c65e730a977baff25af9c75d5103feaf56
unknown <mohdarif013579@gmail.com>
2022-12-03 18:17:19 +0530

    connect to mongodb

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-// Middleware
-// It is used for modify response
-// Easy to apply
-const express = require("express");
-const app = express();
-const requestMidddleware = require("./middleware");
-const route = express.Router();
-// app.use(requestMidddleware); // for all route apply middleware
+// connect nodejs server to nodejs
+const {MongoClient} = require('mongodb');
+// path of mongodb 
+const url ='mongodb://localhost:27017';
+let dataBase ='E-commerce';
+const client = new MongoClient(url);
 
-route.use(requestMidddleware)
-app.get("/", (req, res) => {
-  res.send("Welcome to Home Page");
-});
-
-app.get("/users", (req, res) => {
-  res.send("Welcome to Users Page");
-});
-route.get("/about", (req, res) => {
-  res.send("Welcome to About Page");
-});
-route.get("/contact", (req, res) => {
-  res.send("Welcome to contact Page");
-});
-app.use('/', route)
-
-app.listen(3000);
-
-// Types of Middleware
-// 1. Application middleware
-// 2. Router Level middleware
-// 3. Error handling Middleware
-// 4. Build-in middleware
-// 5. Third party middleware
-
-// Route Level middleware
-
-// * Route Level middleware
-// * Application middleware on single route
-// * Make middleware in different file
-// * Apply Middleware in the group of route
+async function getData() {
+    let result = await client.connect();
+    let db =result.db(dataBase);
+    // collection name
+    let collection = db.collection('products');
+    let response = await collection.find({}).toArray()
+    console.log(response);
+}
+getData()
