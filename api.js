5b137f7682a789659e7ba264c38c7386256af1da
unknown <mohdarif013579@gmail.com>
2022-12-06 11:33:40 +0530

    post api with nodejs

--- api.js
+++ api.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const dbConnect = require("./mongodb");
 const app = express();
+// connect with nodejs
+app.use(express.json());
 
 app.get("/", async (res, resp) => {
   let data = await dbConnect();
   data = await data.find().toArray();
-   resp.send(data);
+  resp.send(data);
+});
+app.post("/", async (req, resp) => {
+  // data add into node js using frontend
+  let data = await dbConnect();
+  let result = await data.insertOne(req.body);
+  resp.send(result);
 });
 
 app.listen(5000);
