8609ba52b602dd00a1915f1ba874253f4c7f4287
unknown <mohdarif013579@gmail.com>
2022-12-20 17:06:28 +0530

    Delete api and put api

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dbConnect = require("./mongodb");
+const mongodb = require("mongodb");
 const app = express();
 // connect with nodejs
 app.use(express.json());
@@ -10,10 +11,38 @@ app.get("/", async (res, resp) => {
   resp.send(data);
 });
 app.post("/", async (req, resp) => {
-  // data add into node js using frontend
+  // data add into node js using fr  ontend
   let data = await dbConnect();
   let result = await data.insertOne(req.body);
   resp.send(result);
 });
+
+app.put("/:name", async (req, res) => {
+  let data = await dbConnect();
+  let result = await data.updateOne(
+    // condition
+    {
+      // name: "note max 5",
+      // name: req.body.name,
+      name:req.params.name
+    },
+    {
+      // static dta update
+      // $set: { price: 090 },
+      $set: req.body,
+    }
+  );
+
+  res.send({ result: "update" });
+});
+
+app.delete('/:id', async(req,res) => {
+  console.log(req.params.id);
+  const data = await dbConnect();
+  const result = await data.deleteOne({_id: new mongodb.ObjectId(req.params.id)})
+   res.send(result)
+})
+
+
 
 app.listen(5000);
